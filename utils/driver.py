40352ac51bdadd06c251731b7bfed5905cf0d8eb
Egor Kostan <igorkostan@gmail.com>
2019-08-25 14:16:33 -0700

    Update driver.py

--- utils/driver.py
+++ utils/driver.py
@@ -6,18 +6,26 @@ from appium import webdriver
 
 
 class Driver:
+	"""
+	Appium Desired Capabilities: http://appium.io/docs/en/writing-running-appium/caps/
+	"""
 
 	def __init__(self):
-
 		self._desired_capabilities = {
 			"platformName": "android",
 			"deviceName": "Android Tablet",
 			"appPackage": "com.android.calculator2",
-			"appActivity": "com.android.calculator2.Calculator"
+			"appActivity": "com.android.calculator2.Calculator",
+			"automationName": "Appium",
+			"platformVersion": "7.0",
 		}
 
+		print("Opening test app: {}...".format(self._desired_capabilities["appActivity"]))
 		self._instance = webdriver.Remote("http://127.0.0.1:4723/wd/hub", self._desired_capabilities)
 
+	@property
+	def capabilities(self):
+		return self._desired_capabilities
 
 	@property
 	def instance(self):
