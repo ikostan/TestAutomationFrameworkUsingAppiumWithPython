887717042aee8c5913302e024b97ac38f96462a8
Egor Kostan <igorkostan@gmail.com>
2019-08-26 00:49:00 -0700

    Create base_element.py

---
+++ drivers/element/base_element.py
@@ -0,0 +1,37 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BaseElement:
+
+	def __init__(self, driver, locator: tuple):
+		self._driver = driver
+		self._locator = locator
+		self._element = self._set_element()
+
+	def _set_element(self):
+		return WebDriverWait(self._driver, 5).until(
+			EC.presence_of_element_located(self._locator))
+
+	def tap(self):
+		"""
+		Tap on app element
+		:return:
+		"""
+		element = WebDriverWait(self._driver, 5).until(
+			EC.element_to_be_clickable(self._locator))
+		element.click()
+		return None
+
+	@property
+	def is_visible(self):
+		"""
+		Returns Boolean.
+		Checks if element visible
+		:return:
+		"""
+		return self._element.is_displayed()
