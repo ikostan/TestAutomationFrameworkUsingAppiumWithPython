fdb6766f45a1c907a2beb3dc47c0e45a1fc146ff
Egor Kostan <igorkostan@gmail.com>
2019-08-26 04:00:58 -0700

    Update addition_test.py

--- tests/calculator_tests/addition_test.py
+++ tests/calculator_tests/addition_test.py
@@ -4,12 +4,17 @@
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
-import unittest
+import allure
+from tests.calculator_tests.base_testcase import BaseTestCase
 
-from utils.driver import Driver
 
-
-class TestAdditionCase(unittest.TestCase):
+@allure.epic('Android Native App')
+@allure.parent_suite('End To End')
+@allure.suite("Calculator Test Suite")
+@allure.sub_suite("Positive Tests")
+@allure.feature("Addition Calculation")
+@allure.story('Addition Button')
+class TestAdditionCase(BaseTestCase):
 
 	"""
 	This suite tests the current code in the calculator_test.
@@ -42,16 +47,129 @@ class TestAdditionCase(unittest.TestCase):
 	Source: https://mozilla.github.io/calculator/test/?grep=Unit%20Tests%20Addition
 	"""
 
-	def setUp(self) -> None:
-		self.driver = Driver()
-		self.driver.set_capability("appPackage", "com.android.calculator2")
-		self.driver.set_capability("appActivity", "com.android.calculator2.Calculator")
-		self.driver.driver_instance.implicitly_wait(3)
-
-	def tearDown(self) -> None:
-		if self.driver.driver_instance:
-			self.driver.driver_instance.quit()
-			self.driver = None
-
-	def test_something(self):
+	def test_able_to_add_two_positive_integers(self):
+		"""
+		Should be able to add two positive integers numbers ‣
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
 		pass
+
+	def test_able_to_add_a_negative_integer_to_a_positive_floating_point_number(self):
+		"""
+		Should be able to add a negative integer to a positive floating point number
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_a_floating_point_number_to_an_integer(self):
+		"""
+		Should be able to add a floating point number to an integer
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_an_integer_to_a_floating_point_number(self):
+		"""
+		Should be able to add an integer to a floating point number
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_two_floating_point_numbers(self):
+		"""
+		Should be able to add two floating point numbers
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_a_negative_integer_and_zero(self):
+		"""
+		Should be able to add a negative integer and zero
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_zero_and_a_positive_integer(self):
+		"""
+		Should be able to add zero and a positive integer
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_a_negative_integer_with_a_positive_number(self):
+		"""
+		Should be able to add a negative integer with a positive number
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_two_large_positive_integers(self):
+		"""
+		Should be able to add two large positive integers
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_a_negative_floating_point_and_a_positive_integer(self):
+		"""
+		Should be able to add a negative floating point and a positive integer
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_a_positive_integer_to_the_results_of_a_previous_operation(self):
+		"""
+		Should be able to add a positive integer to the results of a previous operation
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_a_positive_floating_point_number_to_the_results_of_a_previous_operation(self):
+		"""
+		Should be able to add a positive floating point number to the results of a previous operation
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_a_floating_point_number_with_many_decimal_places_to_a_previous_result(self):
+		"""
+		Should be able to add a floating point number with many decimal places to a previous result
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
+	def test_able_to_add_a_large_integer_to_a_previous_result(self):
+		"""
+		Should be able to add a large integer to a previous result
+		:return:
+		"""
+		allure.dynamic.title("")
+		allure.dynamic.severity(allure.severity_level.BLOCKER)
+		pass
+
