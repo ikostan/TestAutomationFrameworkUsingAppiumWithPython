b43b080308b47d2834dee99dc1b4fc2a94a7eff3
Egor Kostan <igorkostan@gmail.com>
2019-08-27 13:13:01 -0700

    Create multi_touch_test.py

---
+++ tests/calculator_tests/multi_touch_test.py
@@ -0,0 +1,30 @@
+"""Multi Touch Test Case"""
+
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+from tests.calculator_tests.base_testcase import BaseTestCase
+
+
+class TestMultiTouchCase(BaseTestCase):
+	"""
+	Multi Touch Test Case
+	Perform tests with a multi touch action sequence
+
+	Documentation:
+
+	An Overview of the TouchAction / MultiAction API:
+	http://appium.io/docs/en/writing-running-appium/touch-actions/
+
+	Multi Touch Perform:
+	http://appium.io/docs/en/commands/interactions/touch/multi-touch-perform/
+
+	multi_action.py:
+	https://github.com/appium/python-client/blob/master/appium/webdriver/common/multi_action.py
+
+	touch_action.py:
+	https://github.com/appium/python-client/blob/master/appium/webdriver/common/touch_action.py
+	"""
+	pass
+
