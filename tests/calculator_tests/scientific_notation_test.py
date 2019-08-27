3f3fa61f4e1b5db254ad37cc116337eec2402f62
Egor Kostan <igorkostan@gmail.com>
2019-08-27 16:09:34 -0700

    Update scientific_notation_test.py

--- tests/calculator_tests/scientific_notation_test.py
+++ tests/calculator_tests/scientific_notation_test.py
@@ -1,3 +1,4 @@
+"""Scientific Notation Conversion"""
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
@@ -5,7 +6,13 @@
 from tests.calculator_tests.base_testcase import BaseTestCase
 
 
-class MyTestCase(BaseTestCase):
+@allure.epic('Android Native App')
+@allure.parent_suite('Functional Test')
+@allure.suite("Calculator Test Suite")
+@allure.sub_suite("Positive Tests")
+@allure.feature("Scientific Notation")
+@allure.story('Scientific Notation Conversion')
+class TestScientificNotationConversionCase(BaseTestCase):
 
 	def test_something(self):
 		pass
