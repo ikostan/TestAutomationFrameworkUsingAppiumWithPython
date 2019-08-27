f2f5fb18843a3021b2abc7f832ff365a4687d5d0
Egor Kostan <igorkostan@gmail.com>
2019-08-26 23:48:01 -0700

    Create scientific_notation_test.py

---
+++ tests/calculator_tests/scientific_notation_test.py
@@ -0,0 +1,15 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+from tests.calculator_tests.base_testcase import BaseTestCase
+
+
+class MyTestCase(BaseTestCase):
+
+	def test_something(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
