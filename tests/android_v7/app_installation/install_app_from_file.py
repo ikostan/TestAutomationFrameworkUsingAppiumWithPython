08d4a6b9378b8b0c8bb09e3b98b54d6f52e87811
Egor Kostan <igorkostan@gmail.com>
2019-08-28 15:44:14 -0700

    Create install_app_from_file.py

---
+++ tests/android_v7/app_installation/install_app_from_file.py
@@ -0,0 +1,22 @@
+"""Android App Test: apk package installation"""
+
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+import unittest
+import allure
+
+
+@allure.epic('Selendroid Test App')
+@allure.parent_suite('Non Functional Test')
+@allure.suite("Selendroid App Test Suite")
+@allure.sub_suite("Positive Tests")
+@allure.feature("App Installation")
+@allure.story('App Installation From File')
+class TestInstallAppFromFileCase(unittest.TestCase):
+	"""
+	Android App Test: apk package installation
+	App source: http://search.maven.org/remotecontent?filepath=io/selendroid/selendroid-test-app/0.17.0/selendroid-test-app-0.17.0.apk
+	"""
+	pass
