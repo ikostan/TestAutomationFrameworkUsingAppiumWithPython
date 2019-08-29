27beb493b4b381ff4ffd1c157c8c163787dd3a61
Egor Kostan <igorkostan@gmail.com>
2019-08-29 16:50:10 -0700

    Update multi_touch_test.py

--- tests/android_v7/calculator_tests/multi_touch_test.py
+++ tests/android_v7/calculator_tests/multi_touch_test.py
@@ -5,6 +5,10 @@
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 import allure
+
+from appium.webdriver.common.multi_action import MultiAction
+from appium.webdriver.common.touch_action import TouchAction
+
 from tests.android_v7.calculator_tests.calculator_base_testcase import AndroidCalculatorBaseTestCase
 
 
@@ -35,4 +39,22 @@ class TestMultiTouchCase(AndroidCalculatorBaseTestCase):
 	https://github.com/appium/python-client/blob/master/appium/webdriver/common/touch_action.py
 	"""
 	pass
+
+	def test_multi_touch_action(self):
+		"""
+		Test multi touch action
+		:return:
+		"""
+
+		with allure.step("Enter following: 12"):
+			touch_action_1 = TouchAction().press(self.app.digits[1]).release()
+			touch_action_2 = TouchAction().press(self.app.digits[2]).release()
+	
+			multi_touch_action = MultiAction(self.app.driver)
+			multi_touch_action.add(touch_action_2, touch_action_1)
+			multi_touch_action.perform()
+
+			with allure.step("Verify screen output"):
+				assert self.app.screen_formula.label == '12'
+
 
