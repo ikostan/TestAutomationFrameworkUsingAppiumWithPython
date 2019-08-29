3c3a4aca4d2fb395bc91a96fbe010c2e96eb2093
Egor Kostan <igorkostan@gmail.com>
2019-08-29 16:02:55 -0700

    Update clr_button_test.py

--- tests/android_v7/calculator_tests/clr_button_test.py
+++ tests/android_v7/calculator_tests/clr_button_test.py
@@ -40,12 +40,15 @@ class TestClrBtnCase(AndroidCalculatorBaseTestCase):
 		allure.dynamic.title("Clear the screen after inserting a negative floating point number test")
 		allure.dynamic.severity(allure.severity_level.BLOCKER)
 
-		with allure.step("Clear the screen after inserting a negative floating point number"):
-			self.enter_number(-12.3)
+		digit = -12.3
+		with allure.step("Clear the screen after inserting a negative floating point number: {}".format(digit)):
+			self.enter_digit(digit)
 			self.app.equal.tap()
 			self.app.clr.tap()
-			assert self.app.screen_formula.label == ''
-			assert self.app.screen_result.label == ''
+
+			with allure.step("Check screen output"):
+				assert self.app.screen_formula.label == ''
+				assert self.app.screen_result.label == ''
 
 	def test_clear_the_screen_after_inserting_an_positive_floating_point_number(self):
 		"""
@@ -56,12 +59,15 @@ class TestClrBtnCase(AndroidCalculatorBaseTestCase):
 		allure.dynamic.title("Clear the screen after inserting an positive floating point number test")
 		allure.dynamic.severity(allure.severity_level.BLOCKER)
 
-		with allure.step("clear the screen after inserting an positive floating point number"):
-			self.enter_number(12.3)
+		digit = 12.3
+		with allure.step("clear the screen after inserting an positive floating point number: {}".format(digit)):
+			self.enter_digit(digit)
 			self.app.equal.tap()
 			self.app.clr.tap()
-			assert self.app.screen_formula.label == ''
-			assert self.app.screen_result.label == ''
+
+			with allure.step("Check screen output"):
+				assert self.app.screen_formula.label == ''
+				assert self.app.screen_result.label == ''
 
 	def test_clear_the_screen_after_inserting_a_negative_integer(self):
 		"""
@@ -72,12 +78,15 @@ class TestClrBtnCase(AndroidCalculatorBaseTestCase):
 		allure.dynamic.title("Clear the screen after inserting a negative integer test")
 		allure.dynamic.severity(allure.severity_level.BLOCKER)
 
-		with allure.step("Clear the screen after inserting a negative integer"):
-			self.enter_number(-123)
+		digit = -123
+		with allure.step("Clear the screen after inserting a negative integer: {}".format(digit)):
+			self.enter_digit(digit)
 			self.app.equal.tap()
 			self.app.clr.tap()
-			assert self.app.screen_formula.label == ''
-			assert self.app.screen_result.label == ''
+
+			with allure.step("Check screen output"):
+				assert self.app.screen_formula.label == ''
+				assert self.app.screen_result.label == ''
 
 	def test_clear_the_screen_after_inserting_a_positive_integer(self):
 		"""
@@ -88,12 +97,15 @@ class TestClrBtnCase(AndroidCalculatorBaseTestCase):
 		allure.dynamic.title("Clear the screen after inserting a positive integer test")
 		allure.dynamic.severity(allure.severity_level.BLOCKER)
 
-		with allure.step("Clear the screen after inserting a positive integer"):
-			self.enter_number(123)
+		digit = 123
+		with allure.step("Clear the screen after inserting a positive integer: {}".format(digit)):
+			self.enter_digit(digit)
 			self.app.equal.tap()
 			self.app.clr.tap()
-			assert self.app.screen_formula.label == ''
-			assert self.app.screen_result.label == ''
+
+			with allure.step("Check screen output"):
+				assert self.app.screen_formula.label == ''
+				assert self.app.screen_result.label == ''
 
 	def test_clear_to_be_pressed_multiple_times(self):
 		"""
@@ -104,12 +116,15 @@ class TestClrBtnCase(AndroidCalculatorBaseTestCase):
 		allure.dynamic.title("Clear to be pressed multiple times test")
 		allure.dynamic.severity(allure.severity_level.BLOCKER)
 
-		with allure.step("Clear to be pressed multiple times"):
-			self.enter_number(123456789)
+		digit = 123456789
+		with allure.step("Clear {} by pressing CLR".format(digit)):
+			self.enter_digit(digit)
 			self.app.equal.tap()
 			self.app.clr.tap()
-			assert self.app.screen_formula.label == ''
-			assert self.app.screen_result.label == ''
+
+			with allure.step("Check screen output"):
+				assert self.app.screen_formula.label == ''
+				assert self.app.screen_result.label == ''
 
 	def test_clear_after_inserting_a_many_digit_floating_point_number(self):
 		"""
@@ -120,12 +135,15 @@ class TestClrBtnCase(AndroidCalculatorBaseTestCase):
 		allure.dynamic.title("Clear after inserting a many digit floating point number test")
 		allure.dynamic.severity(allure.severity_level.BLOCKER)
 
-		with allure.step("Clear after inserting a many digit floating point number"):
-			self.enter_number(1234.56789)
+		digit = 1234.56789
+		with allure.step("Clear after inserting a many digit floating point number: {}".format(digit)):
+			self.enter_digit(digit)
 			self.app.equal.tap()
 			self.app.clr.tap()
-			assert self.app.screen_formula.label == ''
-			assert self.app.screen_result.label == ''
+
+			with allure.step("Check screen output"):
+				assert self.app.screen_formula.label == ''
+				assert self.app.screen_result.label == ''
 
 	def test_clear_after_inserting_a_negative_many_digit_floating_point_number(self):
 		"""
@@ -136,12 +154,15 @@ class TestClrBtnCase(AndroidCalculatorBaseTestCase):
 		allure.dynamic.title("Clear after inserting a negative many digit floating point number test")
 		allure.dynamic.severity(allure.severity_level.BLOCKER)
 
-		with allure.step("Clear after inserting a negative many digit floating point number"):
-			self.enter_number(-1234.56789)
+		digit = -1234.56789
+		with allure.step("Clear after inserting a negative many digit floating point number: {}".format(digit)):
+			self.enter_digit(digit)
 			self.app.equal.tap()
 			self.app.clr.tap()
-			assert self.app.screen_formula.label == ''
-			assert self.app.screen_result.label == ''
+
+			with allure.step("Check screen output"):
+				assert self.app.screen_formula.label == ''
+				assert self.app.screen_result.label == ''
 
 	def test_clear_after_inserting_a_large_negative_integer(self):
 		"""
@@ -152,12 +173,15 @@ class TestClrBtnCase(AndroidCalculatorBaseTestCase):
 		allure.dynamic.title("Clear after inserting a large negative integer test")
 		allure.dynamic.severity(allure.severity_level.BLOCKER)
 
-		with allure.step("Clear after inserting a large negative integer"):
-			self.enter_number(-123456789)
+		digit = -123456789
+		with allure.step("Clear after inserting a large negative integer: {}".format(digit)):
+			self.enter_digit(digit)
 			self.app.equal.tap()
 			self.app.clr.tap()
-			assert self.app.screen_formula.label == ''
-			assert self.app.screen_result.label == ''
+
+			with allure.step("Check screen output"):
+				assert self.app.screen_formula.label == ''
+				assert self.app.screen_result.label == ''
 
 	def test_clear_after_inserting_a_large_integer(self):
 		"""
@@ -168,11 +192,14 @@ class TestClrBtnCase(AndroidCalculatorBaseTestCase):
 		allure.dynamic.title("Clear after inserting a large integer test")
 		allure.dynamic.severity(allure.severity_level.BLOCKER)
 
-		with allure.step("Clear after inserting a large integer"):
-			self.enter_number(123456789)
+		digit = 123456789
+		with allure.step("Clear after inserting a large integer: {}".format(digit)):
+			self.enter_digit(digit)
 			self.app.equal.tap()
 			self.app.clr.tap()
-			assert self.app.screen_formula.label == ''
-			assert self.app.screen_result.label == ''
+
+			with allure.step("Check screen output"):
+				assert self.app.screen_formula.label == ''
+				assert self.app.screen_result.label == ''
 
 
