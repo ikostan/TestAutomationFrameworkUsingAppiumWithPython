241ad5e52dc1b6c67479cdca61576d0281955fd5
Egor Kostan <igorkostan@gmail.com>
2019-08-30 14:35:36 -0700

    Update open_source_license_test.py

--- tests/android_v7/calculator_tests/open_source_license_test.py
+++ tests/android_v7/calculator_tests/open_source_license_test.py
@@ -5,6 +5,9 @@
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 import allure
+
+from appium.webdriver.common.touch_action import TouchAction
+
 from tests.android_v7.calculator_tests.calculator_base_testcase import AndroidCalculatorBaseTestCase
 
 
@@ -19,4 +22,43 @@ class TestOpenSourceLicenseCase(AndroidCalculatorBaseTestCase):
 	Android Calculator App Test: Open Source License Test
 	Test that open source license is acceptable and can be displayed
 	"""
-	pass
+
+	def test_open_source_license(self):
+		"""
+		1. Open Calculator App
+		2. Open "Open Source License" page/view
+		3. Verify Title
+		4. Close it and go back to calculator
+		5. Close calculator app
+		:return:
+		"""
+
+		self.app = self.app.open_license()
+
+		assert self.app.header == 'Notices for files:'
+
+		assert self.app.crc == 'CRCalc'
+
+		assert self.app.full_license_txt == self.sample_license_txt
+
+		assert self.app.title == 'Open source licenses'
+
+		TouchAction(self.app.driver).tap(self.app.arrow_btn).wait(5).perform()
+
+	@property
+	def sample_license_txt(self) -> str:
+		"""
+		Returns license text from stored/source file
+		:return:
+		"""
+
+		license_file_name = 'C:/Users/superadmin/Documents/GitHub/' \
+		                    'TEST_AUTOMATION_FRAMEWORK_USING_APPIUM_WITH_PYTHON/' \
+		                    'files/open_source_license.txt'
+		license_txt = ''
+
+		with open(license_file_name, "r", encoding="utf-8") as f:
+			for line in f:
+				license_txt += line
+
+		return license_txt
