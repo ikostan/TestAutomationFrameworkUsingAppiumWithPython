20f1dd49062b8be3f8415ee6820b6d2fa251627e
Egor Kostan <igorkostan@gmail.com>
2019-08-31 16:30:23 -0700

    Create services_page_locator.py

---
+++ tests/web_app_tests/parabank_test/page_locators/services_page_locator.py
@@ -0,0 +1,14 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+from page_locators.base_page_locator import BasePageLocator
+
+
+class ServicesPageLocator(BasePageLocator):
+	'''
+	Holds all relevant locators for 'Services' page web elements.
+	Each locator is a tuple.
+	Separate the locator strings from the place where they are being used.
+	'''
+	pass
