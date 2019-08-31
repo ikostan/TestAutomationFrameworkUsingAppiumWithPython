277a9f1987c7a6cc95dc77ba1b5f4cca20d4aaf0
Egor Kostan <igorkostan@gmail.com>
2019-08-31 16:30:12 -0700

    Create forgot_login_info_page_locator.py

---
+++ tests/web_app_tests/parabank_test/page_locators/forgot_login_info_page_locator.py
@@ -0,0 +1,28 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+from selenium.webdriver.common.by import By
+from page_locators.base_page_locator import BasePageLocator
+from page_locators.base_personal_info_page_locator import BasePersonalInfoPageLocator
+
+
+class ForgotLoginInfoPageLocator(BasePageLocator, BasePersonalInfoPageLocator):
+	'''
+	Holds all relevant locators for 'Forgot Login Info' page web elements.
+	Each locator is a tuple.
+	Separate the locator strings from the place where they are being used.
+	'''
+
+	FIND_MY_LOGIN_INFO_BUTTON = (By.XPATH,
+	                             '//input[contains(@value, "Find My Login Info")]')
+
+	# USERNAME = (By.XPATH, '//*[contains(@b, "Username:")]')
+	# PASSWORD = (By.XPATH, '//*[contains(@b, "Password:")]')
+
+	USERNAME_PASSWORD = (By.XPATH, '//*[@id="rightPanel"]/p[2]')
+
+	ERROR_TITLE = (By.XPATH, '//*[@id="rightPanel"]/h1')
+
+	ERROR_MESSAGE = (By.XPATH, '//*[@id="rightPanel"]/p')
+
