c1d4f422efe53f7db7212dff55ddf847b38a963a
Egor Kostan <igorkostan@gmail.com>
2019-08-31 16:42:14 -0700

    Create no_such_user.py

---
+++ tests/web_app_tests/parabank_test/expected_results/users/invalid_users_templates/no_such_user.py
@@ -0,0 +1,24 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+class NoSuchUser:
+
+	FIRST_NAME = 'None'
+	LAST_NAME = 'Anonymous'
+
+	ADDRESS = 'Nowhere'
+	CITY = 'Narnia'
+	STATE = 'Whoops'
+	ZIP_CODE = '000000'
+
+	PHONE = '000000'
+
+	USERNAME = 'noname'
+	PASSWORD = 'nopassword'
+
+	SSN = '000000000'
+
+	INIT_BALANCE = 0.00
+	MIN_BALANCE = 0.00
