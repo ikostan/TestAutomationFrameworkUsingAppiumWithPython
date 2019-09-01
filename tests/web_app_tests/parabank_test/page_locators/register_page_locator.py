eb3b0c6fa0737aa8fcd2f8239c7d80af5b4605b3
Egor Kostan <igorkostan@gmail.com>
2019-08-31 21:50:56 -0700

    Update register_page_locator.py

--- tests/web_app_tests/parabank_test/page_locators/register_page_locator.py
+++ tests/web_app_tests/parabank_test/page_locators/register_page_locator.py
@@ -1,18 +1,20 @@
+"""Register Page Locator Class"""
+
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 from selenium.webdriver.common.by import By
-from page_locators.base_page_locator import BasePageLocator
-from page_locators.base_personal_info_page_locator import BasePersonalInfoPageLocator
+
+from tests.web_app_tests.parabank_test.page_locators.base_page_locator import BasePageLocator
 
 
 class RegisterPageLocator(BasePageLocator, BasePersonalInfoPageLocator):
-	'''
+	"""
 	Holds all relevant locators for 'Register' page web elements.
 	Each locator is a tuple.
 	Separate the locator strings from the place where they are being used.
-	'''
+	"""
 
 	USERNAME_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[10]/td[1]/b')
 	USERNAME_INPUT = (By.XPATH, '//*[@id="customer.username"]')
