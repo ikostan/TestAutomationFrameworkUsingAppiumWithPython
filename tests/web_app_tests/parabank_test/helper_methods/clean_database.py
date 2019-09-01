91cb6f4dff79c7596a2a45432d52abd2351e47dd
Egor Kostan <igorkostan@gmail.com>
2019-09-01 02:31:47 -0700

    Update clean_database.py

--- tests/web_app_tests/parabank_test/helper_methods/clean_database.py
+++ tests/web_app_tests/parabank_test/helper_methods/clean_database.py
@@ -8,35 +8,36 @@ import time
 import allure
 
 from tests.config import Config
-from tests.web_app_tests.parabank_test.helper_methods.open_web_browser import open_web_browser
 from tests.web_app_tests.parabank_test.page_object_models.admin_page_model import AdminPageModel
-from tests.web_app_tests.parabank_test.expected_results.page_content.admin_page_content import AdminPageContent
 
 
-def clean_database(config: Config):
+def clean_database(driver, config: Config):
 	"""
 	Clean and initialise database.
 	Does not check for any errors.
 	:return:
 	"""
 
-	page_model = AdminPageModel
-	page_content = AdminPageContent
-
 	print("Open web browser")
-	page = open_web_browser(config=config,
-	                        page_model=page_model,
-	                        page_content=page_content)
+	page = AdminPageModel(config=config,
+	                      driver=driver,
+	                      implicit_wait_time=5,
+	                      explicit_wait_time=10)
+	# Open Web page
+	with allure.step("Open Admin web page"):
+		page.go()
 
 	with allure.step("Press 'CLEAN' button"):
 		print("\nCleaning database...")
-		page.hit_clean_button()
+		page.clean_button.click_on()
 		time.sleep(2)
 
 	with allure.step("Press 'INITIALIZE' button"):
 		print("\nInitializing database...")
-		page.hit_initialize_button()
+		page.initialize_button.click_on()
 		time.sleep(2)
 
 	with allure.step("Close web browser"):
-		page.quit()
+		print("\n5. Close web browser...")
+		# page.quit()
+		pass
