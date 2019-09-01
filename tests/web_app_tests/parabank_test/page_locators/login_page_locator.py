a75be395715a762ee1ad97417e2416613da2f6cc
Egor Kostan <igorkostan@gmail.com>
2019-08-31 21:51:01 -0700

    Update login_page_locator.py

--- tests/web_app_tests/parabank_test/page_locators/login_page_locator.py
+++ tests/web_app_tests/parabank_test/page_locators/login_page_locator.py
@@ -1,17 +1,20 @@
+"""Login Page Locator Class"""
+
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 from selenium.webdriver.common.by import By
-from page_locators.base_page_locator import BasePageLocator
+
+from tests.web_app_tests.parabank_test.page_locators.base_page_locator import BasePageLocator
 
 
 class LoginPageLocator(BasePageLocator):
-	'''
+	"""
 	Holds all relevant locators for 'Login' page web elements.
 	Each locator is a tuple.
 	Separate the locator strings from the place where they are being used.
-	'''
+	"""
 
 	ERROR_TITLE = (By.XPATH, '//*[@id="rightPanel"]/h1')
 	ERROR_MESSAGE = (By.XPATH, '//*[@id="rightPanel"]/p')
