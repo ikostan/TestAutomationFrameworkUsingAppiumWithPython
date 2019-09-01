7d8025cef31bf5911c8bc74cbad1c0beea71754b
Egor Kostan <igorkostan@gmail.com>
2019-08-31 21:51:22 -0700

    Update admin_page_locator.py

--- tests/web_app_tests/parabank_test/page_locators/admin_page_locator.py
+++ tests/web_app_tests/parabank_test/page_locators/admin_page_locator.py
@@ -1,17 +1,20 @@
+"""Admin Page Locator Class"""
+
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 from selenium.webdriver.common.by import By
-from page_locators.base_page_locator import BasePageLocator
+
+from tests.web_app_tests.parabank_test.page_locators.base_page_locator import BasePageLocator
 
 
 class AdminPageLocator(BasePageLocator):
-	'''
+	"""
 	Holds all relevant locators for 'Admin' page web elements.
 	Each locator is a tuple.
 	Separate the locator strings from the place where they are being used.
-	'''
+	"""
 
 	INITIALIZE_BUTTON = (By.XPATH, '//button[contains(text(),\'Initialize\')]')
 
