25d914f506a3232fc36485526f9e5fb8ee2dc194
Egor Kostan <igorkostan@gmail.com>
2019-08-31 22:03:45 -0700

    Update forgot_login_info_page_locator.py

--- tests/web_app_tests/parabank_test/page_locators/forgot_login_info_page_locator.py
+++ tests/web_app_tests/parabank_test/page_locators/forgot_login_info_page_locator.py
@@ -7,21 +7,29 @@
 from selenium.webdriver.common.by import By
 
 from tests.web_app_tests.parabank_test.page_locators.base_page_locator import BasePageLocator
+from tests.web_app_tests.parabank_test.page_locators.base_personal_info_page_locator import BasePersonalInfoPageLocator
 
 
-class ForgotLoginInfoPageLocator(BasePageLocator, BasePersonalInfoPageLocator):
+class ForgotLoginInfoPageLocator(BasePageLocator):
 	"""
+	Forgot Login Info Page Locator Class
+
 	Holds all relevant locators for 'Forgot Login Info' page web elements.
 	Each locator is a tuple.
 	Separate the locator strings from the place where they are being used.
 	"""
 
+	PERSONAL_INFO = BasePersonalInfoPageLocator
+
 	FIND_MY_LOGIN_INFO_BUTTON = (By.XPATH,
 	                             '//input[contains(@value, "Find My Login Info")]')
 
-	USERNAME_PASSWORD = (By.XPATH, '//*[@id="rightPanel"]/p[2]')
+	USERNAME_PASSWORD = (By.XPATH,
+	                     '//*[@id="rightPanel"]/p[2]')
 
-	ERROR_TITLE = (By.XPATH, '//*[@id="rightPanel"]/h1')
+	ERROR_TITLE = (By.XPATH,
+	               '//*[@id="rightPanel"]/h1')
 
-	ERROR_MESSAGE = (By.XPATH, '//*[@id="rightPanel"]/p')
+	ERROR_MESSAGE = (By.XPATH,
+	                 '//*[@id="rightPanel"]/p')
 
