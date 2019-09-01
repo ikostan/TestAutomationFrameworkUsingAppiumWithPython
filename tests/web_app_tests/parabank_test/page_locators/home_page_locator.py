5f8851ab0ec26373b926c253630316f6a3b58e10
Egor Kostan <igorkostan@gmail.com>
2019-08-31 21:50:59 -0700

    Update home_page_locator.py

--- tests/web_app_tests/parabank_test/page_locators/home_page_locator.py
+++ tests/web_app_tests/parabank_test/page_locators/home_page_locator.py
@@ -1,17 +1,20 @@
+"""Home Page Locator Class"""
+
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 from selenium.webdriver.common.by import By
-from page_locators.base_page_locator import BasePageLocator
+
+from tests.web_app_tests.parabank_test.page_locators.base_page_locator import BasePageLocator
 
 
 class HomePageLocator(BasePageLocator):
-	'''
+	"""
 	Holds all relevant locators for 'HOME' page web elements.
 	Each locator is a tuple.
 	Separate the locator strings from the place where they are being used.
-	'''
+	"""
 
 	# ATM Services
 	ATM_SERVICES_TITLE = (By.XPATH, '//*[@id="rightPanel"]/'
