2ca931d6e13925c31b99cb12c44b4798c684b82a
Egor Kostan <igorkostan@gmail.com>
2019-08-31 21:50:45 -0700

    Update about_page_locator.py

--- tests/web_app_tests/parabank_test/page_locators/about_page_locator.py
+++ tests/web_app_tests/parabank_test/page_locators/about_page_locator.py
@@ -1,17 +1,19 @@
+"""About Page Locator Class"""
+
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 from selenium.webdriver.common.by import By
-from page_locators.base_page_locator import BasePageLocator
+from tests.web_app_tests.parabank_test.page_locators.base_page_locator import BasePageLocator
 
 
 class AboutPageLocator(BasePageLocator):
-	'''
+	"""
 	Holds all relevant locators for 'ABOUT' page web elements.
 	Each locator is a tuple.
 	Separate the locator strings from the place where they are being used.
-	'''
+	"""
 
 	DESCRIPTION_TITLE = (By.XPATH, '//*[@id="rightPanel"]/h1')
 
