0e8bf6d9f90242c0adb1dfdd299e49c9663ff58b
Egor Kostan <igorkostan@gmail.com>
2019-08-31 21:50:53 -0700

    Update services_page_locator.py

--- tests/web_app_tests/parabank_test/page_locators/services_page_locator.py
+++ tests/web_app_tests/parabank_test/page_locators/services_page_locator.py
@@ -1,14 +1,17 @@
+"""Services Page Locator Class"""
+
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
-from page_locators.base_page_locator import BasePageLocator
+from tests.web_app_tests.parabank_test.page_locators.base_page_locator import BasePageLocator
 
 
 class ServicesPageLocator(BasePageLocator):
-	'''
+	"""
 	Holds all relevant locators for 'Services' page web elements.
 	Each locator is a tuple.
 	Separate the locator strings from the place where they are being used.
-	'''
+	"""
 	pass
+
