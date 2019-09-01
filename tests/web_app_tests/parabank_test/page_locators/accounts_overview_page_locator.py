f1cabb95877f6efc0f6715c19c656541867528f2
Egor Kostan <igorkostan@gmail.com>
2019-08-31 21:51:16 -0700

    Update accounts_overview_page_locator.py

--- tests/web_app_tests/parabank_test/page_locators/accounts_overview_page_locator.py
+++ tests/web_app_tests/parabank_test/page_locators/accounts_overview_page_locator.py
@@ -1,13 +1,20 @@
+"""Accounts Overview Page Locator Class"""
+
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 from selenium.webdriver.common.by import By
-from page_locators.base_page_locator import BasePageLocator
-from expected_results.page_content.account_overview_content import AccountOverviewContent
+
+from tests.web_app_tests.parabank_test.page_locators.base_page_locator import BasePageLocator
 
 
 class AccountsOverviewPageLocator(BasePageLocator):
+	"""
+	Holds all relevant locators for 'Accounts Overview' page web elements.
+	Each locator is a tuple.
+	Separate the locator strings from the place where they are being used.
+	"""
 
 	HEADER = (By.XPATH, '//h1[@innertext=\'{}\']'.format(AccountOverviewContent.HEADER))
 
