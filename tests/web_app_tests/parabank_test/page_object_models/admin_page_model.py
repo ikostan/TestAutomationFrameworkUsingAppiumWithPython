431c5d63c97af6c7296cbec781f5ffc442cfe370
Egor Kostan <igorkostan@gmail.com>
2019-08-31 21:37:07 -0700

    Create admin_page_model.py

---
+++ tests/web_app_tests/parabank_test/page_object_models/admin_page_model.py
@@ -0,0 +1,47 @@
+"""Admin Page Model Class"""
+
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+from utils.driver import Driver
+
+from tests.config import Config
+from tests.web_app_tests.parabank_test.page_locators.admin_page_locator import AdminPageLocator
+from tests.web_app_tests.parabank_test.page_object_models.base_page_model import BasePageModel
+from tests.web_app_tests.parabank_test.expected_results.page_content.admin_page_content import AdminPageContent
+
+
+class AdminPageModel(BasePageModel):
+	"""
+	Admin Page Model Class
+
+	The page object pattern intends creating an object for each web page.
+	By following this technique a layer of separation between the test code and technical implementation is created.
+	"""
+
+	def __init__(self, config: Config, driver: Driver, implicit_wait_time, explicit_wait_time):
+
+		super().__init__(config, driver, implicit_wait_time, explicit_wait_time)
+		self._url = config.base_url + AdminPageContent.URL
+		self._locator = AdminPageLocator
+
+	def initialize_button(self):
+		"""
+		Returns Initialize button
+		:return:
+		"""
+
+		locator = self._locator.INITIALIZE_BUTTON
+		element = self.create_web_element(locator)
+		return element
+
+	def clean_button(self):
+		"""
+		Returns Clean button
+		:return:
+		"""
+
+		locator = self._locator.CLEAN_BUTTON
+		element = self.create_web_element(locator)
+		return element
