c148d1cdc43d244cd98242bc23a9cf7bf7ba62f1
Egor Kostan <igorkostan@gmail.com>
2019-09-02 14:07:51 -0700

    Update customer_login_model.py

--- tests/web_app_tests/parabank_test/page_object_models/customer_login_model.py
+++ tests/web_app_tests/parabank_test/page_object_models/customer_login_model.py
@@ -8,10 +8,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from tests.web_app_tests.parabank_test.element_object_model.element import Element
-from tests.web_app_tests.parabank_test.page_object_models.login_page_model import LoginPageModel
 from tests.web_app_tests.parabank_test.page_locators.customer_login_locator import CustomerLoginLocator
 from tests.web_app_tests.parabank_test.expected_results.page_content.login_page_content import LoginPageContent
-from tests.web_app_tests.parabank_test.page_object_models.accounts_overview_page_model import AccountsOverviewPageModel
 from tests.web_app_tests.parabank_test.expected_results.page_content.accounts_overview_page_content import \
 	AccountsOverviewPageContent
 
@@ -135,19 +133,20 @@ class CustomerLoginModel:
 		"""
 		locator = self._locator.CUSTOMER_LOGIN_BUTTON
 		element = self.create_web_element(locator)
-		current_url = self._driver.current_url()
+		current_url = self._driver.current_url
 		element.click_on()
 		WebDriverWait(self._driver, self._explicit_wait_time).until(EC.url_changes(current_url))
 
-		if self._driver.current_url() == self._config.base_url + AccountsOverviewPageContent.URL:
-
+		if self._driver.current_url == self._config.base_url + AccountsOverviewPageContent.URL:
+			from tests.web_app_tests.parabank_test.page_object_models.accounts_overview_page_model import \
+				AccountsOverviewPageModel
 			return AccountsOverviewPageModel(config=self._config,
 			                                 driver=self._driver,
 			                                 implicit_wait_time=5,
 			                                 explicit_wait_time=10)
 
-		if self._driver.current_url() == self._config.base_url + LoginPageContent.URL:
-
+		if self._driver.current_url == self._config.base_url + LoginPageContent.URL:
+			from tests.web_app_tests.parabank_test.page_object_models.login_page_model import LoginPageModel
 			return LoginPageModel(config=self._config,
 			                      driver=self._driver,
 			                      implicit_wait_time=5,
