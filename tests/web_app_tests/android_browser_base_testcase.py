180bf65fa267ab0dcae83b81be81fa828ae025c0
Egor Kostan <igorkostan@gmail.com>
2019-09-02 14:08:29 -0700

    Update android_browser_base_testcase.py

--- tests/web_app_tests/android_browser_base_testcase.py
+++ tests/web_app_tests/android_browser_base_testcase.py
@@ -1,4 +1,4 @@
-"""Base Web App Test Case"""
+"""Base Web App Test Case Class"""
 
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
@@ -16,21 +16,22 @@ from utils.get_args_from_cli import get_args
 
 class AndroidBrowserBaseTestCase(unittest.TestCase):
     """
-    Test basic web app functionality
+    Base Web App Test Case Class
+    Take care of basic test setup
     """
 
     @classmethod
     def setUpClass(cls) -> None:
 
         with allure.step("Start Appium Service"):
-            # that you can use to programmatically start/stop an Appium server.
+            # How to programmatically start/stop an Appium server.
             # Source: https://stackoverflow.com/questions/51734382/how-to-start-appium-server-programmatically-in-python
             # Source: https://github.com/appium/python-client/blob/master/appium/webdriver/appium_service.py
             # Source: https://discuss.appium.io/t/launching-and-stopping-appium-server-programmtically/700/2
             os.system("start /B start cmd.exe @cmd /k appium --relaxed-security")
 
         with allure.step("Set Driver to None"):
-            cls._driver = None
+            cls.driver = None
 
         with allure.step("Get args from CLI"):
             cls.args = get_args()
@@ -38,10 +39,10 @@ class AndroidBrowserBaseTestCase(unittest.TestCase):
     @classmethod
     def tearDownClass(cls) -> None:
         with allure.step("Close Page Model > Set driver to None"):
-            if cls._driver:
-                if cls._driver.driver_instance:
-                    cls._driver.driver_instance.quit()
-                cls._driver = None
+            if cls.driver:
+                if cls.driver.driver_instance:
+                    cls.driver.driver_instance.quit()
+                cls.driver = None
 
         with allure.step("Stop Appium Service"):
             # Stop Appium Service
@@ -51,34 +52,34 @@ class AndroidBrowserBaseTestCase(unittest.TestCase):
     def setUp(self) -> None:
 
         with allure.step("Instantiate Driver"):
-            self._driver = Driver()
+            self.driver = Driver()
 
         with allure.step("Set desired capabilities"):
-            self._driver.set_capability("platformName",
-                                        self.args["platformName"])
+            self.driver.set_capability("platformName",
+                                       self.args["platformName"])
 
-            self._driver.set_capability("deviceName",
-                                        self.args["deviceName"])
+            self.driver.set_capability("deviceName",
+                                       self.args["deviceName"])
 
-            self._driver.set_capability("automationName",
-                                        self.args["automationName"])
+            self.driver.set_capability("automationName",
+                                       self.args["automationName"])
 
-            self._driver.set_capability("browserName",
-                                        "Chrome")
+            self.driver.set_capability("browserName",
+                                       "Chrome")
 
-            self._driver.set_capability('chromedriverExecutable',
-                                        'C:\\Users\\superadmin\\Documents\\GitHub\\'
-                                        'TEST_AUTOMATION_FRAMEWORK_USING_APPIUM_WITH_PYTHON\\'
-                                        'drivers\\chromedriver\\v2_44\\chromedriver.exe')
+            self.driver.set_capability('chromedriverExecutable',
+                                       'C:\\Users\\superadmin\\Documents\\GitHub\\'
+                                       'TEST_AUTOMATION_FRAMEWORK_USING_APPIUM_WITH_PYTHON\\'
+                                       'drivers\\chromedriver\\v2_44\\chromedriver.exe')
 
         # NOTE: In addCleanup, the first in, is executed last.
         with allure.step("Add clean up methods"):
-            self.addCleanup(self._driver.driver_instance.quit)
+            self.addCleanup(self.driver.driver_instance.quit)
             self.addCleanup(self.snap_shot)
 
         with allure.step("Get driver/page instance"):
-            self._driver.driver_instance.implicitly_wait(3)
-            self.page = self._driver.driver_instance
+            self.driver.driver_instance.implicitly_wait(3)
+            self.driver = self.driver.driver_instance
 
     def snap_shot(self):
         """
@@ -90,13 +91,13 @@ class AndroidBrowserBaseTestCase(unittest.TestCase):
                 # Source: https://stackoverflow.com/questions/12024848/
                 # automatic-screenshots-when-test-fail-by-selenium-webdriver-in-python
                 file_name = "screenshot_{}.png".format(self.id())
-                self._driver.driver_instance.get_screenshot_as_file(file_name)
+                self.driver.get_screenshot_as_file(file_name)
                 allure.attach.file('./' + file_name, attachment_type=AttachmentType.PNG)
                 os.remove(file_name)
 
                 # Get Appium Server Logs:
                 # Source: https://appium.readthedocs.io/en/stable/en/commands/session/logs/get-log/#get-logs
-                appium_server_log = self._driver.driver_instance.get_log('server')
+                appium_server_log = self.driver.get_log('server')
                 file_name = "./appium_server_log.txt"
                 with open(file_name, "w") as f:
                     for dictionary in appium_server_log:
