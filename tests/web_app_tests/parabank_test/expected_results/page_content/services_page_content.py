f7b4b94f04789822d3ef4da8104c2f6efe074468
Egor Kostan <igorkostan@gmail.com>
2019-09-02 14:09:34 -0700

    Update services_page_content.py

--- tests/web_app_tests/parabank_test/expected_results/page_content/services_page_content.py
+++ tests/web_app_tests/parabank_test/expected_results/page_content/services_page_content.py
@@ -1,13 +1,17 @@
+"""Services Page Content Class"""
+
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
-
-from expected_results.page_content.base_page_content import BasePageContent
+from tests.web_app_tests.parabank_test.expected_results.page_content.base_page_content import BasePageContent
 
 
 class ServicesPageContent(BasePageContent):
+	"""Services Page Content Class"""
 
 	TITLE = BasePageContent.TITLE + 'Services'
 
-	URL = BasePageContent.URL + 'services.htm'
+	BASE_URL = 'services.htm'
+
+	URL = BasePageContent.URL + BASE_URL
