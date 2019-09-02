7e8223141ba7547f99b48eb115676202ce6ee115
Egor Kostan <igorkostan@gmail.com>
2019-09-02 14:08:55 -0700

    Update accounts_overview_page_content.py

--- tests/web_app_tests/parabank_test/expected_results/page_content/accounts_overview_page_content.py
+++ tests/web_app_tests/parabank_test/expected_results/page_content/accounts_overview_page_content.py
@@ -1,11 +1,14 @@
+"""Accounts Overview Page Content Class"""
+
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
-from expected_results.page_content.base_page_content import BasePageContent
+from tests.web_app_tests.parabank_test.expected_results.page_content.base_page_content import BasePageContent
 
 
 class AccountsOverviewPageContent(BasePageContent):
+	"""Accounts Overview Page Content Class"""
 
 	TITLE = BasePageContent.TITLE + 'Accounts Overview'
 
