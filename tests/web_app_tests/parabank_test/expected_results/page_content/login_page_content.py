c51400bb4d2e1c1460ad7fafd2b6b01e66a7df53
Egor Kostan <igorkostan@gmail.com>
2019-09-02 14:09:25 -0700

    Update login_page_content.py

--- tests/web_app_tests/parabank_test/expected_results/page_content/login_page_content.py
+++ tests/web_app_tests/parabank_test/expected_results/page_content/login_page_content.py
@@ -1,11 +1,14 @@
+"""Login Page Content Class"""
+
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
-from expected_results.page_content.base_page_content import BasePageContent
+from tests.web_app_tests.parabank_test.expected_results.page_content.base_page_content import BasePageContent
 
 
 class LoginPageContent(BasePageContent):
+	"""Login Page Content Class"""
 
 	TITLE_ERROR = BasePageContent.TITLE + 'Error'
 
@@ -14,6 +17,4 @@ class LoginPageContent(BasePageContent):
 	ERROR_TITLE = 'Error!'
 	NO_SUCH_USER_ERROR_MESSAGE = 'The username and password could not be verified.'
 	EMPTY_FIELDS_ERROR_MESSAGE = 'Please enter a username and password.'
-
-
 
