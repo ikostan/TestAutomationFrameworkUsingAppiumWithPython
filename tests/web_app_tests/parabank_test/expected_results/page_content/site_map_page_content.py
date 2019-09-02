dc7feb57f457d6292d82df1d995053f2d94b244d
Egor Kostan <igorkostan@gmail.com>
2019-09-02 14:09:37 -0700

    Update site_map_page_content.py

--- tests/web_app_tests/parabank_test/expected_results/page_content/site_map_page_content.py
+++ tests/web_app_tests/parabank_test/expected_results/page_content/site_map_page_content.py
@@ -1,10 +1,20 @@
+"""Site Map Page Content Class"""
+
 #  Created by Egor Kostan.
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
-
-from expected_results.page_content.base_page_content import BasePageContent
+from tests.web_app_tests.parabank_test.expected_results.page_content.base_page_content import BasePageContent
 
 
 class SiteMapPageContent(BasePageContent):
-	pass
+	"""
+	Site Map Page Content Class
+	Holds expected context values for About web page items
+	"""
+
+	TITLE = BasePageContent.TITLE + 'Site Map'
+
+	BASE_URL = 'sitemap.htm'
+
+	URL = BasePageContent.URL + BASE_URL
