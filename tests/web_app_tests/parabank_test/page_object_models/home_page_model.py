309cb15b6a25dd07d3cb5d1e4583eec6db40ad2c
Egor Kostan <igorkostan@gmail.com>
2019-09-04 21:43:12 -0700

    Update home_page_model.py

--- tests/web_app_tests/parabank_test/page_object_models/home_page_model.py
+++ tests/web_app_tests/parabank_test/page_object_models/home_page_model.py
@@ -5,15 +5,14 @@
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 from utils.driver import Driver
-
 from tests.config import Config
-from tests.web_app_tests.parabank_test.element_object_model.element import Element
+
 from tests.web_app_tests.parabank_test.page_locators.home_page_locator import HomePageLocator
-from tests.web_app_tests.parabank_test.page_object_models.base_page_model import BasePageModel
+from tests.web_app_tests.parabank_test.page_object_models.base_page_object_model import BasePageObjectModel
 from tests.web_app_tests.parabank_test.expected_results.page_content.home_page_content import HomePageContent
 
 
-class HomePageModel(BasePageModel):
+class HomePageModel(BasePageObjectModel):
 	"""
 	Home Page Model Class
 
@@ -21,9 +20,13 @@ class HomePageModel(BasePageModel):
 	By following this technique a layer of separation between the test code and technical implementation is created.
 	"""
 
-	def __init__(self, config: Config, driver: Driver, implicit_wait_time, explicit_wait_time):
-		super().__init__(config, driver, implicit_wait_time, explicit_wait_time)
+	def __init__(self, config: Config, driver: Driver, explicit_wait_time):
+
+		super().__init__(config=config,
+		                 driver=driver,
+		                 explicit_wait_time=explicit_wait_time)
 		self._url = config.base_url + HomePageContent.URL
+		self._locator = HomePageLocator
 
 	@property
 	def atm_title(self):
@@ -31,50 +34,46 @@ class HomePageModel(BasePageModel):
 		Returns title from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver,
-		                  self.explicit_wait_time,
-		                  HomePageLocator.ATM_SERVICES_TITLE)
-
-		txt = element.text
-		return txt
+		locator = self._locator.ATM_SERVICES_TITLE
+		return self.get_text_property(locator)
 
 	@property
 	def atm_withdraw_funds_text(self):
 		"""
-		Returns withdraw_funds_text from ATM Services section
+		Returns withdraw funds text from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.WITHDRAW_FUNDS)
-		txt = element.text
-		return txt
+		locator = self._locator.WITHDRAW_FUNDS
+		return self.get_text_property(locator)
 
 	@property
-	def atm_withdraw_funds_formated_href(self):
+	def atm_withdraw_funds_formatted_href(self):
 		"""
-		Returns withdraw_funds_href from ATM Services section
+		Returns withdraw funds href from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.WITHDRAW_FUNDS)
+		locator = self._locator.WITHDRAW_FUNDS
+		element = self.create_web_element(locator)
 		href = super()._formatted_url(element.element_href)
 		return href
 
 	@property
 	def atm_transfer_funds_text(self):
 		"""
-		Returns transfer_funds_text from ATM Services section
+		Returns transfer funds text from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.ATM_TRANSFER_FUNDS)
-		txt = element.text
-		return txt
+		locator = self._locator.ATM_TRANSFER_FUNDS
+		return self.get_text_property(locator)
 
 	@property
-	def atm_transfer_funds_formated_href(self):
+	def atm_transfer_funds_formatted_href(self):
 		"""
 		Returns transfer_funds_href from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.ATM_TRANSFER_FUNDS)
+		locator = self._locator.ATM_TRANSFER_FUNDS
+		element = self.create_web_element(locator)
 		href = super()._formatted_url(element.element_href)
 		return href
 
@@ -84,9 +83,8 @@ class HomePageModel(BasePageModel):
 		Returns check_balances_text from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.CHECK_BALANCES)
-		txt = element.text
-		return txt
+		locator = self._locator.CHECK_BALANCES
+		return self.get_text_property(locator)
 
 	@property
 	def atm_check_balances_formated_href(self):
@@ -94,7 +92,8 @@ class HomePageModel(BasePageModel):
 		Returns check_balances_href from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.CHECK_BALANCES)
+		locator = self._locator.CHECK_BALANCES
+		element = self.create_web_element(locator)
 		href = super()._formatted_url(element.element_href)
 		return href
 
@@ -104,9 +103,8 @@ class HomePageModel(BasePageModel):
 		Returns make_deposits_text from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.MAKE_DEPOSITS)
-		txt = element.text
-		return txt
+		locator = self._locator.MAKE_DEPOSITS
+		return self.get_text_property(locator)
 
 	@property
 	def atm_make_deposits_formated_href(self):
@@ -114,7 +112,8 @@ class HomePageModel(BasePageModel):
 		Returns make_deposits_href from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.MAKE_DEPOSITS)
+		locator = self._locator.MAKE_DEPOSITS
+		element = self.create_web_element(locator)
 		href = super()._formatted_url(element.element_href)
 		return href
 
@@ -124,9 +123,8 @@ class HomePageModel(BasePageModel):
 		Returns title from online_services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.ONLINE_SERVICES_TITLE)
-		txt = element.text
-		return txt
+		locator = self._locator.ONLINE_SERVICES_TITLE
+		return self.get_text_property(locator)
 
 	@property
 	def bill_pay_formated_href(self):
@@ -134,7 +132,8 @@ class HomePageModel(BasePageModel):
 		Returns bill_pay href from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.BILL_PAY)
+		locator = self._locator.BILL_PAY
+		element = self.create_web_element(locator)
 		href = super()._formatted_url(element.element_href)
 		return href
 
@@ -144,9 +143,8 @@ class HomePageModel(BasePageModel):
 		Returns title from bill_pay section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.BILL_PAY)
-		txt = element.text
-		return txt
+		locator = self._locator.BILL_PAY
+		return self.get_text_property(locator)
 
 	@property
 	def account_history_formated_href(self):
@@ -154,7 +152,8 @@ class HomePageModel(BasePageModel):
 		Returns account_history href from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.ACCOUNT_HISTORY)
+		locator = self._locator.ACCOUNT_HISTORY
+		element = self.create_web_element(locator)
 		href = super()._formatted_url(element.element_href)
 		return href
 
@@ -164,9 +163,8 @@ class HomePageModel(BasePageModel):
 		Returns title from account_history section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.ACCOUNT_HISTORY)
-		txt = element.text
-		return txt
+		locator = self._locator.ACCOUNT_HISTORY
+		return self.get_text_property(locator)
 
 	@property
 	def online_transfer_funds_formated_href(self):
@@ -174,7 +172,8 @@ class HomePageModel(BasePageModel):
 		Returns transfer_funds href from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.ONLINE_TRANSFER_FUNDS)
+		locator = self._locator.ONLINE_TRANSFER_FUNDS
+		element = self.create_web_element(locator)
 		href = super()._formatted_url(element.element_href)
 		return href
 
@@ -184,9 +183,8 @@ class HomePageModel(BasePageModel):
 		Returns title from transfer_funds section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.ONLINE_TRANSFER_FUNDS)
-		txt = element.text
-		return txt
+		locator = self._locator.ONLINE_TRANSFER_FUNDS
+		return self.get_text_property(locator)
 
 	@property
 	def read_more_services_formated_href(self):
@@ -194,7 +192,8 @@ class HomePageModel(BasePageModel):
 		Returns read_more_services href from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.READ_MORE_SERVICES)
+		locator = self._locator.READ_MORE_SERVICES
+		element = self.create_web_element(locator)
 		href = super()._formatted_url(element.element_href)
 		return href
 
@@ -204,9 +203,8 @@ class HomePageModel(BasePageModel):
 		Returns title from read_more_services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.READ_MORE_SERVICES)
-		txt = element.text
-		return txt
+		locator = self._locator.READ_MORE_SERVICES
+		return self.get_text_property(locator)
 
 	@property
 	def read_more_news_formated_href(self):
@@ -214,7 +212,8 @@ class HomePageModel(BasePageModel):
 		Returns read_more_news href from ATM Services section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.READ_MORE_NEWS)
+		locator = self._locator.READ_MORE_NEWS
+		element = self.create_web_element(locator)
 		href = super()._formatted_url(element.element_href)
 		return href
 
@@ -224,9 +223,8 @@ class HomePageModel(BasePageModel):
 		Returns title from read_more_news section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.READ_MORE_NEWS)
-		txt = element.text
-		return txt
+		locator = self._locator.READ_MORE_NEWS
+		return self.get_text_property(locator)
 
 	@property
 	def latest_news_title(self):
@@ -234,6 +232,5 @@ class HomePageModel(BasePageModel):
 		Returns title from latest_news section
 		:return:
 		"""
-		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.LATEST_NEWS)
-		txt = element.text
-		return txt
+		locator = self._locator.LATEST_NEWS
+		return self.get_text_property(locator)
