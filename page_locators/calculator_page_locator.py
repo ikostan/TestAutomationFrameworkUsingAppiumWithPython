d1b85bf8f1b65dbedc4b93654a09073565687088
Egor Kostan <igorkostan@gmail.com>
2019-09-09 21:33:44 -0700

    Update calculator_page_locator.py

--- page_locators/calculator_page_locator.py
+++ page_locators/calculator_page_locator.py
@@ -13,32 +13,32 @@ class CalculatorPageLocator:
     Each locator is a tuple: locator method + locator
     """
     DIGITS = {
-        0: (MobileBy.ID, 'com.android.calculator2:id/digit_0'),
-        1: (MobileBy.ID, 'com.android.calculator2:id/digit_1'),
-        2: (MobileBy.ID, 'com.android.calculator2:id/digit_2'),
-        3: (MobileBy.ID, 'com.android.calculator2:id/digit_3'),
-        4: (MobileBy.ID, 'com.android.calculator2:id/digit_4'),
-        5: (MobileBy.ID, 'com.android.calculator2:id/digit_5'),
-        6: (MobileBy.ID, 'com.android.calculator2:id/digit_6'),
-        7: (MobileBy.ID, 'com.android.calculator2:id/digit_7'),
-        8: (MobileBy.ID, 'com.android.calculator2:id/digit_8'),
-        9: (MobileBy.ID, 'com.android.calculator2:id/digit_9'),
+        0: (MobileBy.ID, 'com.android_native.calculator2:id/digit_0'),
+        1: (MobileBy.ID, 'com.android_native.calculator2:id/digit_1'),
+        2: (MobileBy.ID, 'com.android_native.calculator2:id/digit_2'),
+        3: (MobileBy.ID, 'com.android_native.calculator2:id/digit_3'),
+        4: (MobileBy.ID, 'com.android_native.calculator2:id/digit_4'),
+        5: (MobileBy.ID, 'com.android_native.calculator2:id/digit_5'),
+        6: (MobileBy.ID, 'com.android_native.calculator2:id/digit_6'),
+        7: (MobileBy.ID, 'com.android_native.calculator2:id/digit_7'),
+        8: (MobileBy.ID, 'com.android_native.calculator2:id/digit_8'),
+        9: (MobileBy.ID, 'com.android_native.calculator2:id/digit_9'),
     }
 
-    POINT_BTN = (MobileBy.ID, 'com.android.calculator2:id/dec_point')
-    PLUS_BTN = (MobileBy.ID, 'com.android.calculator2:id/op_add')
-    MINUS_BTN = (MobileBy.ID, 'com.android.calculator2:id/op_sub')
-    MULTIPLICATION_BTN = (MobileBy.ID, 'com.android.calculator2:id/op_mul')
-    DIVISION_BTN = (MobileBy.ID, 'com.android.calculator2:id/op_div')
-    DEL_BTN = (MobileBy.ID, 'com.android.calculator2:id/del')
+    POINT_BTN = (MobileBy.ID, 'com.android_native.calculator2:id/dec_point')
+    PLUS_BTN = (MobileBy.ID, 'com.android_native.calculator2:id/op_add')
+    MINUS_BTN = (MobileBy.ID, 'com.android_native.calculator2:id/op_sub')
+    MULTIPLICATION_BTN = (MobileBy.ID, 'com.android_native.calculator2:id/op_mul')
+    DIVISION_BTN = (MobileBy.ID, 'com.android_native.calculator2:id/op_div')
+    DEL_BTN = (MobileBy.ID, 'com.android_native.calculator2:id/del')
 
-    CLEAR_BTN = (MobileBy.ID, 'com.android.calculator2:id/clr')
-    EQUAL_BTN = (MobileBy.ID, 'com.android.calculator2:id/eq')
+    CLEAR_BTN = (MobileBy.ID, 'com.android_native.calculator2:id/clr')
+    EQUAL_BTN = (MobileBy.ID, 'com.android_native.calculator2:id/eq')
 
-    DISPLAY = (MobileBy.ID, 'com.android.calculator2:id/display')
-    SCREEN_FORMULA = (MobileBy.ID, 'com.android.calculator2:id/formula')
-    SCREEN_RESULT = (MobileBy.ID, 'com.android.calculator2:id/result')
+    DISPLAY = (MobileBy.ID, 'com.android_native.calculator2:id/display')
+    SCREEN_FORMULA = (MobileBy.ID, 'com.android_native.calculator2:id/formula')
+    SCREEN_RESULT = (MobileBy.ID, 'com.android_native.calculator2:id/result')
 
-    MORE_OPTIONS = (MobileBy.XPATH, '//android.widget.ImageButton[@content-desc="More options"]')
+    MORE_OPTIONS = (MobileBy.XPATH, '//android_native.widget.ImageButton[@content-desc="More options"]')
 
-    OPEN_SOURCE_LICENSE = (MobileBy.ID, 'android:id/title')
+    OPEN_SOURCE_LICENSE = (MobileBy.ID, 'android_native:id/title')
